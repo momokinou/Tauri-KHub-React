b5644bc7b4ba6368c53c1bfe5c7b19dba2b33da9
BuildTools <unconfigured@null.spigotmc.org>
2022-07-03 16:46:52 +0200

    test: test de fetch | mise en pause fetch => cause: CloudFlare + dynamique + setup cookie sur wyr, etc

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,35 @@
   windows_subsystem = "windows"
 )]
 
+use reqwest::header::USER_AGENT;
+
+#[tauri::command]
+async fn get_request() -> Result<(), String> {
+  let user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/78.0$userAgentRandomizer";
+  let url = "https://bato.to/series/89188/ooh-la-la";
+  let client = reqwest::Client::new();
+  let response = client
+    .get(url)
+    .header(USER_AGENT, user_agent)
+    .send()
+    .await
+    .unwrap()
+    .text()
+    .await
+    .or(Err(format!("Failed to GET from '{}'", &url)))?;
+
+
+    println!("{:#?}", response);
+
+    Ok(())
+}
+
 fn main() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
+  .invoke_handler(tauri::generate_handler![
+    get_request
+  ])
     .menu(if cfg!(target_os = "macos") {
       tauri::Menu::os_default(&context.package_info().name)
     } else {
--- src/Components/Inputs.tsx
+++ src/Components/Inputs.tsx
@@ -2,6 +2,7 @@ import { Button, TextInput } from "@mantine/core";
 import { GitHubLogoIcon, NotionLogoIcon } from "@radix-ui/react-icons";
 import { invoke } from '@tauri-apps/api/tauri'
 import { stringify } from "querystring";
+import { fetch } from '@tauri-apps/api/http';
 
 function CustomInput() {
 
@@ -10,6 +11,17 @@ function CustomInput() {
         </>
     );
 
+}
+
+
+// async function getData() {
+//     const response = await fetch('https://bato.to/chapter/1632251');
+//     console.log(response);
+// }
+
+async function getData() {
+    const response = invoke('get_request');
+    console.log(response);
 }
 
 function WebsiteInput() {
@@ -21,9 +33,11 @@ function WebsiteInput() {
                 rightSection={<NotionLogoIcon/>}
                 label="Tape moi ton site web"
             />
-            <Button onClick={() => invoke('connect_to')}>
+            {/* <Button onClick={() => invoke('get_request')}> */}
+            <Button onClick={() => getData()}>
                 Settings
             </Button>
+
         </>
     );
 
